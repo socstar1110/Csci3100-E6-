338792444b00c476a74c37e948e95004d22e4e84
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-11 17:32:05 +0800

    Add files via upload

--- demo-app/src/search.js
+++ demo-app/src/search.js
@@ -18,6 +18,10 @@ const Search =() =>{
     const [username, setUsername] = useState(''); 
     const [selectedValue, setSelectedValue] = useState(''); // this variable indicate which conditon would like to use to serach
     const [action, setAction] = useState(''); // this variable indicate which page we would like to display (course cart or search)
+    const [isLoading, setLoading] = useState(true); // set a loading term to make sure the system fetch the required data before return
+
+    const [searchResult , setSearchResult ] = useState(); // final search result 
+    
 
     const [serachByConditon, SetSerachByConditon] = useState({ 
       Conditon: '', 
@@ -49,19 +53,26 @@ const Search =() =>{
 
 
     function SerachByConditon(event){
+      SetSerachByConditon(prevState => ({ ...prevState, Conditon: selectedValue })); // save the conditon into serachByConditon
+      
       event.preventDefault();
         fetch('http://localhost:80/searchbycondition',{ 
         method:'POST',
         model:'cors',
         headers:{
           'Content-Type':'application/json'
-      },body: JSON.stringify((serachByConditon)) // send modifyCourse object to backend
+      },body: JSON.stringify((serachByConditon)) // send serachByConditon object to backend
       })
-      .then(res => res.text())
-      .then(data => console.log(data))
+      .then(res => res.json())
+      .then(data => {
+        setSearchResult (data) // store data to output
+        setLoading(false)
+      })
     }
 
-    const data = [ //temp data for display 
+    //let data = JSON.parse(localStorage.getItem('MatchCourse')) // fetch back the data from local Storage
+
+    const temp = [ //temp data for display 
       {name:"Software Engineering", id:"4912",code: 'Csci3100', venue:"Lsk", time:"12:30 - 2:15",department:"Computer Science",instructor:"Micheal",capacity:200},
       {name:"Data Structures", id:"4469",code: 'Csci2100', venue:"Yia", time:"9:30 - 11:15",department:"Computer Science",instructor:"Allen",capacity:200}
     ];
@@ -115,7 +126,7 @@ const Search =() =>{
                 </tr>
               </thead>
               <tbody>
-                {data.map((course) => (
+                {temp.map((course) => (
                   <tr>
                     <td style={{padding: '20px'}}><Link to={'/'}>{course.code}</Link></td>
                     <td style={{padding: '20px'}}>{course.name}</td>
@@ -143,140 +154,114 @@ const Search =() =>{
             </div>  
           </div>
         }
-  
-        {/* serach by keyword */}
-          {action == "Search" && selectedValue == "other"&&
+
+        {action == "Search" && (
           <div className = "container d-flex justify-content-center align-items-center" >
-          <div className="search" style={{ display: 'block' }}> 
-            <h6 style={{ display: 'block' }}>Select Search Condition</h6>
-            {/* select the condition */}
-            <select value={selectedValue} onChange={handleSelectChange}>
-                  <option value="other"> other </option>
-                  <option value="department"> department </option>
-                  <option value="instructor"> instructor </option>
-                  <option value="place"> place </option>
-            </select>
-            {/* let user input the keyword in here */}
-            <h6 style={{ display: 'block' }}>Search by keywords...</h6>
-            <label>
-                <input className="search " type="type" placeholder="Search by the course's keywords..." required="required" style={{ width: '300px' }}></input>
-            </label>
-            <button style={{ width: '30px', height: '30px', padding: '0px' }}> <SearchIcon style={{ width: '25px', height: '25px', padding: '0px' }}/> </button>
-          </div>
-        </div>
-        }
-  
-        {action == "Search" && selectedValue == "department"&&
-        <div className = "container d-flex justify-content-center align-items-center" >
-          <div className="search" style={{ display: 'block' }}> 
-            <h6 style={{ display: 'block' }}>Search by department...</h6>
-            <h6 style={{ display: 'block' }}>Select Search Condition</h6>
-            {/* select the condition */}
-            <select value={selectedValue} onChange={handleSelectChange}>
+            <div className="search" style={{ display: 'block' }}> 
+              <h6 style={{ display: 'block' }}>Search by instructor...</h6>
+              <h6 style={{ display: 'block' }}>Select Search Condition</h6>
+              
+              <select value={selectedValue} onChange={handleSelectChange}>
                 <option value="other"> other </option>
                 <option value="department"> department </option>
                 <option value="instructor"> instructor </option>
                 <option value="place"> place </option>
-            </select>
-            {/* provide a option for user  */}
-            <br></br>
-            <select value={serachByConditon.value} onChange={handleSerachByConditonValueChange}>
-            <option >-- Select an option --</option>
-              <option value="Department of Risk Management and Statistics">	Department of Risk Management and Statistics </option>
-              <option value="Department of Computer Science and Engineering"> Department of Computer Science and Engineering </option>
-            </select>
-            <button style={{ width: '30px', height: '30px', padding: '0px' }} onClick={SerachByConditon}> 
-              <SearchIcon style={{ width: '25px', height: '25px', padding: '0px' }}/> 
-            </button>
-          </div>
-        </div>
-        }
+              </select>
+              <br></br>
 
-        {action == "Search" && selectedValue == "instructor" &&
-        <div className = "container d-flex justify-content-center align-items-center" >
-        <div className="search" style={{ display: 'block' }}> 
-          <h6 style={{ display: 'block' }}>Search by instructor...</h6>
-          <h6 style={{ display: 'block' }}>Select Search Condition</h6>
-          {/* select the condition */}
-          <select value={selectedValue} onChange={handleSelectChange}>
-            <option value="other"> other </option>
-            <option value="department"> department </option>
-            <option value="instructor"> instructor </option>
-            <option value="place"> place </option>
-          </select>
-          {/* provide a option for user  */}
-          <select>
-            <option value="">-- Select an option --</option>
-            <option value="Micheal"> Micheal </option>
-            <option value="Ben"> Ben </option>
-          </select>
-          <button style={{ width: '30px', height: '30px', padding: '0px' }}> <SearchIcon style={{ width: '25px', height: '25px', padding: '0px' }}/> </button>
+              {selectedValue == "other" &&
+                <div>
+                 <h6 style={{ display: 'block' }}>Search by keywords...</h6>
+                 <label>
+                     <input className="search " type="type" placeholder="Search by the course's keywords..." required="required" style={{ width: '300px' }}></input>
+                 </label>
+                </div>
+              }
+              
+              {selectedValue == "department" && (
+                <select value={serachByConditon.value} onChange={handleSerachByConditonValueChange}> {/*save the selected value into serachByConditon*/}
+                <option >-- Select an option --</option>
+                  <option value="Department of Risk Management and Statistics">	Department of Risk Management and Statistics </option>
+                  <option value="Department of Computer Science and Engineering"> Department of Computer Science and Engineering </option>
+                  <option value="Department of Statistics and Actuarial Science"> Department of Statistics and Actuarial Science </option>
+                  <option value="Department of Chemistry"> Department of Chemistry</option>
+                  <option value="Department of Economics"> Department of Economics</option>
+                  <option value="Department of English"> Department of English</option>
+                  <option value="Department of Linguistics and Modern Languages"> Department of Linguistics and Modern Languages</option>
+                  <option value="Department of Psychology"> Department of Psychology</option>
+                  <option value="Department of History"> Department of History</option>
+                  <option value="Department of Mathematics"> Department of Mathematics</option>
+                  <option value="Department of Sociology"> Department of Sociology</option>
+                  <option value="Department of Urban Planning and Design"> Department of Urban Planning and Design</option>
+                  <option value="General Education"> General Education</option>
+                </select>
+              )}
+
+              {selectedValue == "instructor" &&(
+                <select>
+                <option value="">-- Select an option --</option>
+                <option value="Micheal"> Micheal </option>
+                <option value="Ben"> Ben </option>
+              </select>
+              )}
+
+              {selectedValue == "place"&&(
+                <select>
+                <option value="">-- Select an option --</option>
+                <option value="Yia"> Yia </option>
+                <option value="Lsk"> Lsk </option>
+              </select>
+              )
+              }
+              <button style={{ width: '30px', height: '30px', padding: '0px' }} onClick={SerachByConditon}> <SearchIcon style={{ width: '25px', height: '25px', padding: '0px' }}/> </button>
+            </div>
           </div>
-        </div>
-        }
+          )}
   
-        {action == "Search" && selectedValue == "place" &&
-        <div className = "container d-flex justify-content-center align-items-center" >
-          <div className="search" style={{ display: 'block' }}> 
-            <h6 style={{ display: 'block' }}>Search by location...</h6>
-            <h6 style={{ display: 'block' }}>Select Search Condition</h6>
-            {/* select the condition */}
-            <select value={selectedValue} onChange={handleSelectChange}>
-                <option value="other"> other </option>
-                <option value="department"> department </option>
-                <option value="instructor"> instructor </option>
-                <option value="place"> place </option>
-            </select>
-            {/* provide a option for user  */}
-            <select>
-              <option value="">-- Select an option --</option>
-              <option value="Yia"> Yia </option>
-              <option value="Lsk"> Lsk </option>
-            </select>
-            <button style={{ width: '30px', height: '30px', padding: '0px' }}> <SearchIcon style={{ width: '25px', height: '25px', padding: '0px' }}/> </button>
-          </div>
-        </div>
-        }
 
 
-
-        {action == "Search" &&
-        <div className = "container d-flex justify-content-center align-items-center" >
-          <table>
-              {/* show the search result by a table  */}
-              <thead>
-                <tr>
-                  <th style={{padding: '20px'}}>CourseCode</th>
-                  <th style={{padding: '20px'}}>CourseName</th>
-                  <th style={{padding: '20px'}}>CourseID</th>
-                  <th style={{padding: '20px'}}>Venue</th>
-                  <th style={{padding: '20px'}}>Time</th>
-                  <th style={{padding: '20px'}}>Department</th>
-                  <th style={{padding: '20px'}}>Instructor</th>
-                  <th style={{padding: '20px'}}>Capacity</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((course) => (
+        {action == "Search" && isLoading == false &&
+          <div className = "container d-flex justify-content-center align-items-center" >
+            <table>
+                <thead>
                   <tr>
-                    <td style={{padding: '20px'}}><Link to={'/'}>{course.code}</Link></td>
-                    <td style={{padding: '20px'}}>{course.name}</td>
-                    <td style={{padding: '20px'}}>{course.id}</td>
-                    <td style={{padding: '20px'}}>{course.venue}</td>
-                    <td style={{padding: '20px'}}>{course.time}</td>
-                    <td style={{padding: '20px'}}>{course.department}</td>
-                    <td style={{padding: '20px'}}>{course.instructor}</td>
-                    <td style={{padding: '20px'}}>{course.capacity}</td>
-                    <td style={{padding: '20px'}}>
-                      <button className="addCrouse" style={{ width: '40px', height: '40px', padding: '0px' }} >
-                        <AddIcon className="icon"/>
-                      </button>
-                    </td>
+                    <th style={{padding: '10px'}}>CourseCode</th> {/* add a padding for a clear ui )*/}
+                    <th style={{padding: '10px'}}>CourseName</th>
+                    <th style={{padding: '10px'}}>CourseID</th>
+                    <th style={{padding: '10px'}}>Venue</th>
+                    <th style={{padding: '10px'}}>Data</th>
+                    <th style={{padding: '10px'}}>StartTime</th>
+                    <th style={{padding: '10px'}}>EndTime</th>
+                    <th style={{padding: '10px'}}>Department</th>
+                    <th style={{padding: '10px'}}>Instructor</th>
+                    <th style={{padding: '10px'}}>Capacity</th>
+                    <th style={{padding: '10px'}}>Available</th>
                   </tr>
-                ))}
-              </tbody>
-          </table>
-        </div>
+                </thead>
+                <tbody>
+                  {searchResult .map((course) => (
+                    <tr>
+                      <td style={{padding: '10px'}}><Link to={'/coursedetail/' + course.id}>{course.CourseCode}</Link></td> {/* a link to a acces crouse detail )*/}
+                      <td style={{padding: '10px'}}>{course.CourseName}</td>
+                      <td style={{padding: '10px'}}>{course.CourseId}</td>
+                      <td style={{padding: '10px'}}>{course.Venue}</td>
+                      <td style={{padding: '10px'}}>{course.Data}</td>
+                      <td style={{padding: '10px'}}>{course.StartTime}</td>
+                      <td style={{padding: '10px'}}>{course.EndTime}</td>
+                      <td style={{padding: '10px'}}>{course.Department}</td>
+                      <td style={{padding: '10px'}}>{course.Instructor}</td>
+                      <td style={{padding: '10px'}}>{course.Capacity}</td>
+                      <td style={{padding: '10px'}}>{course.Capacity - course.RegUser.length}</td>
+                      <td style={{padding: '10px'}}>
+                        <button className="addCrouse" style={{ width: '40px', height: '40px', padding: '0px' }} >
+                          <AddIcon className="icon"/>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+          </div>
         }
   
       </div>
