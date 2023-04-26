0ba47cd296935a6ebee96b3416d2b4728ec32f9e
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:34:47 +0800

    Update retrievecourseinformation.js

--- backend/retrievecourseinformation.js
+++ backend/retrievecourseinformation.js
@@ -1,20 +1,45 @@
+// Import required modules and dependencies
 const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./mongoose');
 
-router.post('/retrievecourseinfo', (req, res) => {
-    console.log(req.body);
-    const { courseIds } = req.body;
-    const conditions = courseIds.map((id) => ({ _id: id }));
-    Course.find({ $or: conditions }, { CourseCode: 1, CourseName: 1, Venue: 1, Date: 1, StartTime: 1, EndTime: 1, Instructor: 1 })
-        .then((courses) => {
-            console.log(courses);
-            res.send(courses);
+// Route for removing a course and updating user records
+router.post('/removecourse', (req, res) => {
+    // Find the course with the given ID
+    Course.findOne({ CourseId: req.body['id'] }).then(function (course){
+        console.log(course._id)
+        // Find all users
+        User.find().then(function(user){
+            // Loop through each user
+            for (let i = 0; i < user.length; i++){
+                // If the user has the course in their cart, remove it from their cart
+                if(user[i].CartCourse.includes(course._id)){
+                    const temp = user[i].CartCourse.indexOf(course._id)
+                    console.log(temp)
+                    user[i].CartCourse.splice(temp,1)
+                    user[i].save();
+                }
+                // If the user has the course in their list of registered courses, remove it from their list of registered courses
+                else if(user[i].RegCourse.includes(course._id)){
+                    const temp = user[i].RegCourse.indexOf(course._id)
+                    console.log(temp)
+                    user[i].RegCourse.splice(temp,1)
+                    user[i].save();
+                }
+                // If the user does not have the course in their cart or list of registered courses, do nothing
+                else{
+                    console.log("testing")
+                }
+            }
         })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error retrieving courses');
-        });
-});
+    })
 
+    // Delete the course with the given ID
+    Course.deleteOne({ CourseId: req.body['id'] }).then(function (result) {// Delete the course based on the ID 
+        // Send a success response to the client
+        res.send('Deleted')
+    })
+})
+
+// Export the router object
 module.exports = router;
