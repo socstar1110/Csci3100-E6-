e0fd49530ba1cb84a17ad865413c4b49330fd128
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:21:25 +0800

    Update displayallcourse.js

--- backend/displayallcourse.js
+++ backend/displayallcourse.js
@@ -1,33 +1,41 @@
+// Import the required modules
 const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./mongoose');
 const cookieParser = require('cookie-parser');
-router.use(cookieParser()); //cookies
+router.use(cookieParser()); // Use the 'cookie-parser' middleware to handle cookies
 
+// Define a route handler for POST requests to the '/allcourse' endpoint
 router.post('/allcourse', (req, res) => {
+    // Use the 'find' method of the Course model to retrieve all course records from the database
     Course.find().then(function (result) {
-      const obj = [];
-      for (let i = 0; i < result.length; i++) {
-        obj.push({
-          name: result[i].CourseName,
-          id: result[i].CourseId,
-          code: result[i].CourseCode,
-          venue: result[i].Venue,
-          Date: result[i].Date,
-          StartTime: result[i].StartTime,
-          EndTime: result[i].EndTime,
-          department: result[i].Department,
-          instructor: result[i].Instructor,
-          capacity: result[i].Capacity,
-          available: result[i].Capacity - result[i].RegUser.length,
-        });
-      }
-      res.cookie('mycookie', '123');
-      res.send(obj);
-      console.log('All course information is sent');
+        // Create an array of objects containing the course information extracted from the records
+        const obj = [];
+        for (let i = 0; i < result.length; i++) {
+            obj.push({
+                name: result[i].CourseName,
+                id: result[i].CourseId,
+                code: result[i].CourseCode,
+                venue: result[i].Venue,
+                Date: result[i].Date,
+                StartTime: result[i].StartTime,
+                EndTime: result[i].EndTime,
+                department: result[i].Department,
+                instructor: result[i].Instructor,
+                capacity: result[i].Capacity,
+                available: result[i].Capacity - result[i].RegUser.length,
+            });
+        }
+        // Set a cookie named 'mycookie' with a value of '123'
+        res.cookie('mycookie', '123');
+        // Send the array of course objects in the response
+        res.send(obj);
+        console.log('All course information is sent');
     }).catch(function (error) {
-      console.log(error);
+        // If an error occurs during the process, log the error to the console
+        console.log(error);
     });
-  });
+});
 
+// Export the router for use by other modules
 module.exports = router;
