372d096d7cd011b3358b091e5858a2effb0c7bca
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:32:32 +0800

    Update removeSelected.js

--- backend/removeSelected.js
+++ backend/removeSelected.js
@@ -1,26 +1,31 @@
+// Import required modules and dependencies
 const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./mongoose');
 
+// Route for removing a course from a user's list of registered courses
 router.post('/removeSelected', (req, res) => {
-    console.log(req.body)
+    // Find the course with the given ID
     Course.findOne({CourseId: req.body['id']}).then(function(course){
-        console.log(course._id)
+        // Find the user with the given username
         User.findOne({username: req.body['username']}).then(function(user){
-            console.log(user.RegCourse.indexOf(course._id))
+            // Remove the course from the user's list of registered courses
             user.RegCourse.splice(user.RegCourse.indexOf(course._id), 1)
             user.save()
         })
     })
+    // Find the user with the given username
     User.findOne({username: req.body['username']}).then(function(user){
-        console.log(user._id)
+        // Find the course with the given ID
         Course.findOne({CourseId: req.body['id']}).then(function(course){
-            console.log(course.RegUser.indexOf(user._id))
+            // Remove the user from the course's list of registered users
             course.RegUser.splice(course.RegUser.indexOf(user._id), 1)
             course.save()
+            // Send a success response to the client
             res.send("success")
         })
     })
 })
 
+// Export the router object
 module.exports = router;
