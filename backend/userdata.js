5e252e8d3a69e0fdad4b6f7786d5b5be940c9507
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:36:46 +0800

    Update userdata.js

--- backend/userdata.js
+++ backend/userdata.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./mongoose');
 
-
+// Route to get user data for a given username
 router.post('/userdata',(req,res) =>{
+    // Find the user with the given username
     User.findOne({username:req.body['username']}).then(function(result){
+        // Log the user data to the console for debugging purposes
         console.log(result)
+        // Send the user data to the client
         res.send(result)
     })
-    
 })
 
+// Export the router object
 module.exports = router;
