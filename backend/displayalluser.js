b240680bf822df698c6c916bc359311a1e453a84
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:22:35 +0800

    Update displayalluser.js

--- backend/displayalluser.js
+++ backend/displayalluser.js
@@ -1,9 +1,13 @@
+// Import the required modules
 const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./mongoose');
 
+// Define a route handler for POST requests to the '/alluser' endpoint
 router.post('/alluser', (req, res) => {
+    // Use the 'find' method of the User model to retrieve all user records from the database
     User.find()
+      // Use the 'populate' method to populate the 'RegCourse' and 'CartCourse' properties of the user records with their respective course records
       .populate('RegCourse')
       .populate({
         path: 'CartCourse',
@@ -11,10 +15,12 @@ router.post('/alluser', (req, res) => {
         select: 'CourseCode' // Only select the CourseCode property from the Course collection
       })
       .then(function (result) {
-        const obj = []
+        // Create an array of objects containing the user information extracted from the records
+        const obj = [];
         for (let i = 0; i < result.length; i++) {
-          const cartCourses = result[i].CartCourse.map(cartcourse => cartcourse.CourseCode).join(', '); // Join the course codes with a comma separator
-          const regCourses = result[i].RegCourse.map(course => course.CourseCode).join(', '); // Join the course codes with a comma separator
+          // Join the course codes with a comma separator
+          const cartCourses = result[i].CartCourse.map(cartcourse => cartcourse.CourseCode).join(', ');
+          const regCourses = result[i].RegCourse.map(course => course.CourseCode).join(', ');
           obj.push({
             username: result[i].username,
             password: result[i].password,
@@ -26,13 +32,16 @@ router.post('/alluser', (req, res) => {
             Phone: result[i].Phone,
             CartCourse: cartCourses,
             RegCourse: regCourses
-          })
+          });
         }
-        //console.log(obj)
-        res.send(obj)
-        console.log("All user information is sent")
+        // Send the array of user objects in the response
+        res.send(obj);
+        console.log("All user information is sent");
       }).catch(function (error) {
-        console.log(error)
+        // If an error occurs during the process, log the error to the console
+        console.log(error);
       });
-  })
+});
+
+// Export the router for use by other modules
 module.exports = router;
