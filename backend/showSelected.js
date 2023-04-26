86beeb47c6b08026b33f6ff2a3f8eee302c916ce
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:35:41 +0800

    Update showSelected.js

--- backend/showSelected.js
+++ backend/showSelected.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 const { User, Course } = require('./mongoose');
 const cookieParser = require('cookie-parser');
-router.use(cookieParser()); //cookies
+router.use(cookieParser()); // Use cookie-parser middleware to handle cookies
 
-
+// Route for showing selected courses for a user
 router.post('/showSelected', (req, res) => {
-    //console.log(req.body)
-
+    // Find the user with the given username and populate their list of registered courses
     User.findOne({ username: req.body['username'] })
         .populate('RegCourse')
         .exec()
         .then(result => {
+            // Send the user's list of registered courses to the client
             res.send(result.RegCourse);
         })
         .catch(err => {
+            // Handle any errors that occur
             console.error(err);
             res.status(500).send('An error occurred');
         });
-    //res.send('test')
-
 })
 
+// Export the router object
 module.exports = router;
