97323a4ac3f4b657d1f38ffbdb6b71f71031af31
socstar1110 <125180560+socstar1110@users.noreply.github.com>
2023-04-26 23:29:40 +0800

    Update mongoose.js

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+
+// Define the User schema
 const UserSchema = mongoose.Schema({
     username: { type: String, require: true, unique: true },
     password: { type: String, require: true },
@@ -11,11 +13,13 @@ const UserSchema = mongoose.Schema({
     RegCourse: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course'}]
 })
 
+// Define the Admin schema
 const AdminSchema = mongoose.Schema({
     adminName: { type: String, require: true, unique: true },
     password: { type: String, require: true }
 })
 
+// Define the Course schema
 const CourseSchema = mongoose.Schema({
     CourseCode: { type: String, require: true, unique: true },
     CourseName: { type: String, require: true, unique: true },
@@ -31,8 +35,14 @@ const CourseSchema = mongoose.Schema({
     RegUser: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 })
 
+// Create the User model using the User schema
 const User = mongoose.model('User', UserSchema)
+
+// Create the Course model using the Course schema
 const Course = mongoose.model('Course', CourseSchema)
+
+// Create the Admin model using the Admin schema
 const Admin = mongoose.model('Admin', AdminSchema)
 
+// Export the User, Course, and Admin models
 module.exports ={User, Course,Admin}
